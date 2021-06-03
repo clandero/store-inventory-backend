172eae41bcba553c2c88fff0c63191629b6d4d97
Carlos Landero <clandero@inf.udec.cl>
2021-06-02 22:29:21 -0400

    DELETE validation removed

--- controllers/products-controller.js
+++ controllers/products-controller.js
@@ -95,6 +95,8 @@ const deleteProductById = async (req, res, next) => {
         // if(!errors.isEmpty()){
         //     throw new HttpError('Invalid input, please check your data.',422);
         // }
+        console.log(req.body.id)
+        console.log(mongoose.Types.ObjectId(req.body.id))
         const filter = {"_id":mongoose.Types.ObjectId(req.body.id)}
         console.log(filter);
         const result = await Product.findOneAndDelete(filter)
